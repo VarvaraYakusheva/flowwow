593e8dc8b24e39832588ba35f4470d40263ebcea
VarvaraYakusheva <varvaraakuseva904@gmail.com>
2026-05-11 21:46:41 +0500

    Add files via upload

---
+++ JSMALEVICH1.js
@@ -0,0 +1,31 @@
+const IMAGE_SRCS = [
+
+  "https://raw.githubusercontent.com/VarvaraYakusheva/flowwow/refs/heads/main/1M.png",
+
+  "http://raw.githubusercontent.com/VarvaraYakusheva/flowwow/refs/heads/main/2M.png",
+
+  "https://raw.githubusercontent.com/VarvaraYakusheva/flowwow/refs/heads/main/3M.png",
+
+  "https://raw.githubusercontent.com/VarvaraYakusheva/flowwow/refs/heads/main/4M.png",
+
+  "https://raw.githubusercontent.com/VarvaraYakusheva/flowwow/refs/heads/main/5M.png",
+
+  "https://raw.githubusercontent.com/VarvaraYakusheva/flowwow/refs/heads/main/6M.png"
+
+];
+const IMAGE_ASPECTS = [1, 1, 1, 1, 1, 1];
+const FACE_NAMES = [
+
+  "Черный квадрат",
+
+  "Белое на белом",
+
+  "Красный квадрат",
+
+  "Супрематизм",
+
+  "Англичанин в Москве",
+
+  "Скачет красная конница"
+
+];
